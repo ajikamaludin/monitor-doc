8672123ef0f1b9d5f97e04641579852eceed4c7f
ajikamaludin <aji.kamaludin@bankraya.co.id>
2023-01-25 16:39:57 +0700

    Init New flavor

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,10 +3,12 @@ import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import { ToastContainer, toast } from 'react-toastify'
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import NavLink from '@/Components/NavLink';
 import { Link } from '@inertiajs/inertia-react';
 import MenuItem from '@/Components/SidebarMenuItem';
 import { IconBell, IconBellRing } from '@/Icons';
 import { Inertia } from '@inertiajs/inertia';
+import { NavDropdown, NavItem } from '@/Components/NavItem';
 
 const Notification = ({ notifications, hasUnread }) => {
     const redirect = (item) => {
@@ -51,96 +53,56 @@ export default function Authenticated({ auth, children, flash, notify }) {
     return (
         <div className="min-h-screen bg-base-200 pb-10">
             <nav className="bg-base-100 border-b border-base-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex justify-between h-16">
-                        <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="font-bold text-3xl block h-9 w-auto" />
-                                </Link>
-                            </div>
-
-                            {/* <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
-                                </NavLink>
-                            </div> */}
+                <div className="navbar bg-base-100 rounded-box max-w-7xl m-auto px-6">
+                    <div className="px-2">
+                        <a className="text-xl font-bold">Monitor Doc</a>
+                    </div> 
+                    <div className="sm:flex flex-1 px-6 hidden">
+                        <div className="flex items-stretch">
+                            <NavItem href={route("dashboard")}  active={route().current('dashboard')}>
+                                Dashboard
+                            </NavItem>
+                           <NavDropdown
+                            name="Dokumen"
+                            items={[
+                                {label: "Dokumen", route: 'docs.index'},
+                                {label: "User", route: 'users.index'},
+                            ]}
+                           ></NavDropdown>
+                            <NavItem href={route("users.index")}  active={route().current('users.*')}>
+                                User
+                            </NavItem>
                         </div>
-
-                        <div className="hidden sm:flex sm:items-center sm:ml-6">
-                            <div className="ml-3 relative">
-                                <Notification 
-                                    notifications={notify.notifications} 
-                                    hasUnread={+notify.notification_has_unread > 0 ? true : false}
+                    </div>
+                    <div className="sm:flex justify-end flex-1 px-2 hidden">
+                        {auth.user.name}
+                    </div>
+                    <div className="flex-1 justify-end items-center sm:hidden">
+                        <button
+                            onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
+                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                        >
+                            <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path
+                                    className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
                                 />
-                            </div>
-                            <div className="ml-3 relative">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md bg-base-100 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {auth.user.name}
-
-                                                <svg
-                                                    className="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content width='w-42'>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <div className="-mr-20 flex items-center sm:hidden">
-                            <Notification 
-                                notifications={notify.notifications} 
-                                hasUnread={+notify.notification_has_unread > 0 ? true : false}
-                            />
-                        </div>
-                        <div className="-mr-2 flex items-center sm:hidden">
-                            <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                                <path
+                                    className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
                     </div>
                 </div>
 
+                
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
@@ -168,21 +130,7 @@ export default function Authenticated({ auth, children, flash, notify }) {
                     </div>
                 </div>
             </nav>
-
             <div className='flex flex-row md:mt-5 max-w-7xl mx-auto'>
-                <div className='w-auto hidden md:block'>
-                    <aside className="ml-5 w-64" aria-label="Sidebar">
-                        <div className="overflow-y-auto py-4 px-3 bg-base-100 rounded">
-                            <ul className="space-y-2">
-                                <MenuItem routeName='dashboard' active='dashboard' name='Dashboard' />
-                                <MenuItem routeName='docs.index' active='docs.*' name='Monitoring' />
-                                {auth.user.is_admin === 1 && (
-                                <MenuItem routeName='users.index' active='users.*' name='Users' />
-                                )}
-                            </ul>
-                        </div>
-                    </aside>
-                </div>
 
                 <div className='w-full pt-5 md:pt-0'>
                     <main>{children}</main>
